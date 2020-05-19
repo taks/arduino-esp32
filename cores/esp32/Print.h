df18866d28ada127fb745482d3d634eb13318e87
taks <857tn859@gmail.com>
2020-05-20 01:03:18 +0900

    Add Print::availableForWrite
    same as arduino/Arduino#5789

--- cores/esp32/Print.h
+++ cores/esp32/Print.h
@@ -71,6 +71,10 @@ public:
         return write((const uint8_t *) buffer, size);
     }
 
+    // default to zero, meaning "a single write may block"
+    // should be overriden by subclasses with buffering
+    virtual int availableForWrite() { return 0; }
+
     size_t printf(const char * format, ...)  __attribute__ ((format (printf, 2, 3)));
     size_t print(const __FlashStringHelper *);
     size_t print(const String &);
